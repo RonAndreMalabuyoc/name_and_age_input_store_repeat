d08da812ead0c0696e3eb50441e8c8f9dcd5a867
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-24 17:31:45 +0800

    Made it print through the dictionary but still haven't gotten how to save multiple inputs

--- name_and_age_input_store_repeat.py
+++ name_and_age_input_store_repeat.py
@@ -1,12 +1,14 @@
+user_info = {}
+
 while True:
     while True:
         try:
             Name = input("Please put your name here: ")
             Age = int(input ("Please put your age here: "))
+            user_info['Name'] = Name
+            user_info['age'] = Age
             Retry = input("Will you give us your information again? Put yes if yes, put no if no: ")
             if Retry == "no":
-                print("name:", Name)
-                print("Age:", Age)
                 break
         except:
             print("Invalid input, please try again")
@@ -16,3 +18,5 @@ while True:
         break
     elif Retry != "no":
         print("Good job Buddy!")
+
+print("User info: ", user_info)
