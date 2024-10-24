efac1c3c2bdcbf62157afef9a879d7cc6611f648
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-24 14:57:34 +0800

    Fixed name and age input and also input errors

--- name_and_age_input_store_repeat.py
+++ name_and_age_input_store_repeat.py
@@ -1,12 +1,16 @@
 while True:
     while True:
         try:
-            Age = int(input ("Please put your name here: "))
+            Name = input("Please put your name here: ")
+            Age = int(input ("Please put your age here: "))
             Retry = input("Will you give us your information again? Put yes if yes, put no if no: ")
             if Retry == "no":
+                print("name:", Name)
+                print("Age:", Age)
                 break
         except:
-            print(" hehehe")
+            print("Invalid input, please try again")
+            
 
     if Retry == "no":
         break
